5d5bb89e082ad4ba91337e7b70c5e8bc374d82d7
Nankiewic <nankiewiczpawel@gmail.com>
2020-10-31 23:16:04 +0100

    Initial commit

---
+++ src/main/java/pl/nankiewic/fleetappbackend/FleetappbackendApplication.java
@@ -0,0 +1,13 @@
+package pl.nankiewic.fleetappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetappbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetappbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/nankiewic/fleetappbackend/FleetappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nankiewic.fleetappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetappbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
