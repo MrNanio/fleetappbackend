a2ad955c1df430d1664903680217f3d56c445b74
Paweł Nankiewicz <nankiewic@gmail.com>
2021-09-14 19:52:58 +0200

    refactoring use module

--- src/main/java/pl/nankiewic/fleetappbackend/Controller/UseController.java
+++ src/main/java/pl/nankiewic/fleetappbackend/Controller/UseController.java
@@ -6,8 +6,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import pl.nankiewic.fleetappbackend.DTO.UseDTO;
-import pl.nankiewic.fleetappbackend.Exception.PermissionDeniedException;
-import pl.nankiewic.fleetappbackend.Service.CheckService;
 import pl.nankiewic.fleetappbackend.Service.UseService;
 
 import javax.validation.Valid;
@@ -18,7 +16,6 @@ import javax.validation.Valid;
 @CrossOrigin(origins = "http://localhost:4200")
 public class UseController {
 
-    private final CheckService checkService;
     private final UseService useService;
 
     @PostMapping
@@ -26,9 +23,7 @@ public class UseController {
     public void createVehicleUse(Authentication authentication,
                                  @RequestBody UseDTO useDTO) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToVehicle(userDetails.getUsername(), useDTO.getVehicleId())) {
-            useService.createVehicleUse(useDTO, userDetails.getUsername());
-        } else throw new PermissionDeniedException();
+        useService.createVehicleUse(useDTO, userDetails.getUsername());
     }
 
     @PutMapping
@@ -36,27 +31,21 @@ public class UseController {
     public void updateVehicleUse(Authentication authentication,
                                  @RequestBody @Valid UseDTO useDTO) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToUse(userDetails.getUsername(), useDTO.getId())) {
-            useService.updateVehicleUse(useDTO);
-        } else throw new PermissionDeniedException();
+        useService.updateVehicleUse(useDTO, userDetails.getUsername());
     }
 
     @GetMapping("/v/{id}")
     public Iterable<UseDTO> getUseByVehicleId(@PathVariable Long id,
                                               Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToVehicle(userDetails.getUsername(), id)) {
-            return useService.getUseByVehicle(id);
-        } else throw new PermissionDeniedException();
+        return useService.getUseByVehicle(id, userDetails.getUsername());
     }
 
     @GetMapping("/{id}")
     public UseDTO getUseByUseId(@PathVariable Long id,
                                 Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToUse(userDetails.getUsername(), id)) {
-            return useService.getUseByUseId(id);
-        } else throw new PermissionDeniedException();
+        return useService.getUseByUseId(id, userDetails.getUsername());
     }
 
     @GetMapping
@@ -70,16 +59,12 @@ public class UseController {
                                                        @RequestParam(name = "v") Long vehicleId,
                                                        Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToVehicle(userDetails.getUsername(), vehicleId)) {
-            return useService.getUseByUserAndVehicle(userId, vehicleId);
-        } else throw new PermissionDeniedException();
+        return useService.getUseByUserAndVehicle(userId, vehicleId, userDetails.getUsername());
     }
 
     @DeleteMapping("/{id}")
     public void deleteUse(Authentication authentication, @PathVariable Long id) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToUse(userDetails.getUsername(), id)) {
-            useService.deleteUseById(id);
-        } else throw new PermissionDeniedException();
+        useService.deleteUseById(id, userDetails.getUsername());
     }
 }
