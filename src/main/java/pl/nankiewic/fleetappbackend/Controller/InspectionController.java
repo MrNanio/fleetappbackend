ca9ca5d11b0aa520c6913b7bb3417d2b4842ac15
Paweł Nankiewicz <nankiewic@gmail.com>
2021-09-14 20:36:00 +0200

    refactoring inspection module

--- src/main/java/pl/nankiewic/fleetappbackend/Controller/InspectionController.java
+++ src/main/java/pl/nankiewic/fleetappbackend/Controller/InspectionController.java
@@ -6,8 +6,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import pl.nankiewic.fleetappbackend.DTO.InspectionDTO;
-import pl.nankiewic.fleetappbackend.Exception.PermissionDeniedException;
-import pl.nankiewic.fleetappbackend.Service.CheckService;
 import pl.nankiewic.fleetappbackend.Service.InspectionService;
 
 import javax.validation.Valid;
@@ -19,39 +17,30 @@ import javax.validation.Valid;
 @CrossOrigin(origins = "http://localhost:4200")
 public class InspectionController {
 
-    private final CheckService checkService;
     private final InspectionService inspectionService;
 
     @PostMapping
     public void addInspection(@RequestBody @Valid InspectionDTO inspectionDTO, Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToVehicle(userDetails.getUsername(), inspectionDTO.getVehicleId())) {
-            inspectionService.createInspection(inspectionDTO);
-        } else throw new PermissionDeniedException();
+        inspectionService.createInspection(inspectionDTO, userDetails.getUsername());
     }
 
     @PutMapping
     public void updateInspection(@RequestBody @Valid InspectionDTO inspectionDTO, Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToVehicle(userDetails.getUsername(), inspectionDTO.getVehicleId())) {
-            inspectionService.updateInspection(inspectionDTO);
-        } else throw new PermissionDeniedException();
+        inspectionService.updateInspection(inspectionDTO, userDetails.getUsername());
     }
 
     @GetMapping("/v/{id}")
     public Iterable<InspectionDTO> getInspectionsByVehicle(@PathVariable Long id, Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToVehicle(userDetails.getUsername(), id)) {
-            return inspectionService.getInspectionByVehicle(id);
-        } else throw new PermissionDeniedException();
+        return inspectionService.getInspectionByVehicle(id, userDetails.getUsername());
     }
 
     @GetMapping("/{id}")
     public InspectionDTO getInspectionById(@PathVariable Long id, Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToInspection(userDetails.getUsername(), id)) {
-            return inspectionService.getInspectionById(id);
-        } else throw new PermissionDeniedException();
+        return inspectionService.getInspectionById(id, userDetails.getUsername());
     }
 
     @GetMapping
@@ -63,8 +52,6 @@ public class InspectionController {
     @DeleteMapping("/{id}")
     public void deleteInspection(Authentication authentication, @PathVariable Long id) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        if (checkService.accessToInspection(userDetails.getUsername(), id)) {
-            inspectionService.deleteInspectionById(id);
-        } else throw new PermissionDeniedException();
+        inspectionService.deleteInspectionById(id, userDetails.getUsername());
     }
 }
--- src/main/java/pl/nankiewic/fleetappbackend/Service/InspectionService.java
+++ src/main/java/pl/nankiewic/fleetappbackend/Service/InspectionService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.nankiewic.fleetappbackend.DTO.InspectionDTO;
 import pl.nankiewic.fleetappbackend.Entity.VehicleInspection;
+import pl.nankiewic.fleetappbackend.Exception.PermissionDeniedException;
 import pl.nankiewic.fleetappbackend.Mapper.InspectionMapper;
 import pl.nankiewic.fleetappbackend.Repository.VehicleInspectionRepository;
 
@@ -12,36 +13,47 @@ import javax.persistence.EntityNotFoundException;
 @AllArgsConstructor
 @Service
 public class InspectionService {
+    private final CheckExistAndPermissionComponent checkExistAndPermissionComponent;
     private final VehicleInspectionRepository vehicleInspectionRepository;
     private final InspectionMapper inspectionMapper;
 
-    public void createInspection(InspectionDTO inspectionDTO) {
-        VehicleInspection vehicleInspection = inspectionMapper.vehicleInspectionDtoToEntity(inspectionDTO);
-        vehicleInspectionRepository.save(vehicleInspection);
+    public void createInspection(InspectionDTO inspectionDTO, String email) {
+        if (checkExistAndPermissionComponent.accessToVehicle(email, inspectionDTO.getVehicleId())) {
+            VehicleInspection vehicleInspection = inspectionMapper.vehicleInspectionDtoToEntity(inspectionDTO);
+            vehicleInspectionRepository.save(vehicleInspection);
+        } else throw new PermissionDeniedException();
+
     }
 
-    public void updateInspection(InspectionDTO inspectionDTO) {
-        VehicleInspection vehicleInspection = vehicleInspectionRepository.findById(inspectionDTO.getId()).orElseThrow(
-                () -> new EntityNotFoundException("Inspection not found"));
-        inspectionMapper.updateVehicleInspectionFromDto(vehicleInspection, inspectionDTO);
-        vehicleInspectionRepository.save(vehicleInspection);
+    public void updateInspection(InspectionDTO inspectionDTO, String email) {
+        if (checkExistAndPermissionComponent.accessToVehicle(email, inspectionDTO.getVehicleId())) {
+            VehicleInspection vehicleInspection = vehicleInspectionRepository.findById(inspectionDTO.getId()).orElseThrow(
+                    () -> new EntityNotFoundException("Inspection not found"));
+            inspectionMapper.updateVehicleInspectionFromDto(vehicleInspection, inspectionDTO);
+            vehicleInspectionRepository.save(vehicleInspection);
+        } else throw new PermissionDeniedException();
     }
 
-    public Iterable<InspectionDTO> getInspectionByVehicle(Long id) {
-        return vehicleInspectionRepository.findAllByVehicle(id);
+    public Iterable<InspectionDTO> getInspectionByVehicle(Long id, String email) {
+        if (checkExistAndPermissionComponent.accessToVehicle(email, id)) {
+            return vehicleInspectionRepository.findAllByVehicle(id);
+        } else throw new PermissionDeniedException();
     }
 
-    public InspectionDTO getInspectionById(Long id) {
-
-        return vehicleInspectionRepository.findInspectionById(id);
+    public InspectionDTO getInspectionById(Long id, String email) {
+        if (checkExistAndPermissionComponent.accessToInspection(email, id)) {
+            return vehicleInspectionRepository.findInspectionById(id);
+        } else throw new PermissionDeniedException();
     }
 
     public Iterable<InspectionDTO> getInspectionsByUser(String email) {
         return vehicleInspectionRepository.findAllByVehicleIn(email);
     }
 
-    public void deleteInspectionById(Long id) {
-        vehicleInspectionRepository.deleteById(id);
+    public void deleteInspectionById(Long id, String email) {
+        if (checkExistAndPermissionComponent.accessToInspection(email, id)) {
+            vehicleInspectionRepository.deleteById(id);
+        } else throw new PermissionDeniedException();
     }
 }
 
